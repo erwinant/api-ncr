e1d44d91db12c5582190f3fc7934878888efea6f
erwin antonius <erwin.ant@gmail.com>
2020-02-26 09:10:53 +0700

    text

--- route/rlocation.js
+++ route/rlocation.js
@@ -4,20 +4,36 @@ var router = express.Router();
 
 router.get('/', function (req, res, next) {
     model.Location.findAll({
-        include: [{ model: model.Project },{ model: model.Report }]
+        include: [{ model: model.Project }, { model: model.Report }]
     }).then(result => {
         res.json(result);
     });
 });
 router.post('/cr/', function (req, res, next) {
-    req.body.RowStatus=1;
-    model.Location.findAll({where:req.body}).then((result)=> {
+    req.body.RowStatus = 1;
+    model.Location.findAll({ where: req.body }).then((result) => {
         res.json(result);
-      })
+    })
 });
 router.post('/', function (req, res, next) {
-    model.Location.create(req.body).then((result)=> {
+    model.Location.create(req.body).then((result) => {
         res.json(result);
-      })
+    })
+});
+router.post('/odata/', function (req, res, next) {
+    var parseOData = require("@wesselkuipers/odata-sequelize");
+    var sequelize = require("sequelize");
+    var query = parseOData(
+        "$select=LocationName&$top=4&$filter=LocationName eq 'Basement'",
+        sequelize
+    );
+
+    // Supposing you have your sequelize model
+    model.Location.findAll(query).then((result) => {
+        res.json(result);
+    });
+    // model.Location.create(req.body).then((result) => {
+    //     res.json(result);
+    // })
 });
 module.exports = router;
